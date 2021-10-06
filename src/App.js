59b29eb951f037bca1aa8badbe6dddbd20768c90
Nataliia Pogrebna <natalibunny13@gmail.com>
2021-10-06 13:54:10 +0300

    task1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Phonebook from './components/Phonebook/Phonebook';
+
+function App() {
+  return (
+    <div className="App">
+      <Phonebook />
+      
+    </div>
+  );
+}
+
+export default App;
